392243c9c6a3ba8cf4a1d9f6db7783720e1ca662
Serai Yan <serai@santori.ai>
2024-09-07 05:11:41 +0000

    finished lesson 1

--- index.ts
+++ index.ts
@@ -5,13 +5,175 @@ const input = require('prompt-sync')({sigint: true});
 // https://supermario-game.com/
 
 
-// print statements
-console.log("hello world")
+/////////// print statements ///////////
+// console.log("hello world");
+
+
+// print me a greeting, and goodbye
+
 
 // print a 3x3 grid
 
 
 // print a 3x3 grid, with some tic tac toe being played
 
-const n = input("what's your name? ")
-console.log("hello " + n + "!");
+
+/////////// variables - strings ///////////
+// let a = "shuhua";
+// console.log(a);
+// console.log("hello", a);
+
+
+// say shuhua 10 times in a single line
+
+
+// say shuhua 10 times vertically
+
+
+
+// say shuhua serai 5 times in a single line
+
+
+// say shuhua, then serai, 5 times vertically
+
+
+// variables can be reassigned
+// let a = "shuhua";
+// console.log(a);
+// a = "kelly";
+// console.log(a);
+
+
+/////////// adding strings ///////////
+// let a = "shuhua";
+// let b = "serai";
+// console.log(a + b);
+
+// console.log(a + " " + b);
+
+
+// print out month, date, and year formatted
+// let date = "8";
+// let month = "9";
+// let year = "2024";
+
+
+
+/////////// inputs ///////////
+
+// const n = input("type something")
+// console.log(n);
+
+
+// ask for my name, and say hi to me
+
+
+// ask for my first name, last name, date of birth,
+// month, and year,
+// then say "<name> was born on <month date year>"
+
+
+
+/////////// variables - numbers ///////////
+// let a = 5;
+// let b = 4;
+// console.log(a + b);
+
+
+// operators: + - * / %
+// do the problems in "number problems.png"
+
+
+
+// what's the remainder of 534 dividing 7?
+
+
+
+// how to make 5 + 4 be 54? there are 2 ways!
+// let a = 5;
+// let b = 4;
+// console.log(a + b);
+
+
+
+
+/////////// variables - booleans ///////////
+// let a = true;
+// let b = false;
+// console.log(a);
+// console.log(b);
+
+
+// Calculating booleans using comparisons
+// let a = 5;
+// let b = 4;
+// let c = 1;
+
+// console.log(a == a);
+// console.log(a == b);
+// console.log(a == b + c);
+
+// console.log(a != a);
+// console.log(a != b);
+// console.log(a != b + c);
+
+// console.log(a > b);
+// console.log(b > a);
+// console.log(a > b + c);
+// console.log(a >= b + c);
+// console.log(a <= b);
+// console.log(a <= b + c);
+
+// assigning booleans to variables
+// let d = a == a;
+// console.log(d);
+// d = a == b;
+// console.log(d);
+// d = a == b + c;
+// console.log(d);
+// d = a != b + c;
+// console.log(d);
+
+
+/////////// if statements ///////////
+// let a = 5;
+// let b = 1;
+// if (a > b) {
+//     console.log("a is bigger than b");
+// }
+// console.log("code is done running.")
+
+
+
+
+/////////// boolean and ///////////
+// let a = true;
+// let b = true;
+// console.log(a && b);
+
+
+// // use case - users and passwords
+// let user = "shuhua";
+// let password = "abcde";
+// if (user == "shuhua" && password == "abcde") {
+//     console.log("hello shuhua");
+// } else if (user == "serai" && password == "12345") {
+//     console.log("hello serai");
+// } else {
+//     console.log("we don't know you");
+// }
+
+
+// do the previous code, but input the user and password
+
+
+// do the previous code, input the user and password,
+// and tell me how much money I have in my account.
+// let shuhuaMoney = 120;
+// let seraiMoney = 26;
+
+
+
+// write rock paper scissors
+// let player1 = "rock"
+// let player2 = "scissors"
