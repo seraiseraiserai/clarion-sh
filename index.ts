0ab14057d7e7d85a5fabcbc98f846e2ef0bf4d07
Serai Yan <serai@santori.ai>
2024-09-07 04:31:23 +0000

    first lesson progress

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
+const input = require('prompt-sync')({sigint: true});
+
+
+// play mario
+// https://supermario-game.com/
+
+
+// print statements
 console.log("hello world")
+
+// print a 3x3 grid
+
+
+// print a 3x3 grid, with some tic tac toe being played
+
+const n = input("what's your name? ")
+console.log("hello " + n + "!");
