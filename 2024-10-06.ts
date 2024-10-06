9b9d5ec8236cd714673c7e7dc6fe3f33e3322220
Serai Yan <serai@santori.ai>
2024-10-06 19:22:48 +0000

    10-06 class

--- 2024-10-06.ts
+++ 2024-10-06.ts
@@ -2,28 +2,137 @@ export {};
 import Prompt from "prompt-sync";
 const input = Prompt({sigint: true});
 
+
+// // What do I do?
+// let a = "shuhua";
+// let b = "serai";
+// console.log(a + b);
+
+
+
+// // What do I do?
+// let a = input("what's your name?: ");
+// console.log("hello", a + "!");
+
+///////// variables - numbers ///////////
+// let a = "5";
+// let b = "4";
+
+// console.log(a + b);
+
+
+// operators: + - * / %
+// do the problems in "number problems.png"
+// let a = 5;
+// let b = 2;
+// let c = 3;
+// let d = 7;
+// let e = 9;
+// let f = 4;
+
+// console.log((c * a) / (b + c));
+// console.log((d+e) * b);
+
+// let g = f - d;
+// console.log(g*g*g);
+
+
+
+// // what's the remainder of 534 dividing 7?
+// let a = 534;
+// let b = 7;
+
+
+// console.log(a%b);
+
+
+// how to make 5 + 4 be 54? there are 2 ways!
+// let a = 5;
+// let b = 4;
+// console.log(a + b);
+
+
+
+
+// ///////// variables - booleans ///////////
+// let a = true;
+// let b = false;
+// console.log(a);
+// console.log(b);
+
+
+// Calculating booleans using comparisons
+
+
+// console.log(a == a); // t
+// console.log(a == b); // f
+// console.log(a == b + c); // t
+
+// console.log(a != a); // f
+// console.log(a != b); // t
+// console.log(a != b + c); // f
+
+// console.log(a > b); // t
+// console.log(b > a); // f
+// console.log(a > b + c); // f
+// console.log(a >= b + c); // t
+// console.log(a <= b); // f
+// console.log(a <= b + c); // t
+
+// let a = 5;
+// let b = 4;
+// let c = 1;
+
+// // assigning booleans to variables
+// let d = a == a;
+// console.log(d); // t
+// d = a == b;
+// console.log(d); // f
+// d = a == b + c;
+// console.log(d); // t
+// d = a != b + c;
+// console.log(d); // f
+
+
 /////////// if statements ///////////
-let a = 5;
-let b = 1;
-if (a > b) {
-    console.log("a is bigger than b");
+// let a = 5;
+// let b = 1;
+// if (a > b) {
+//     console.log("a is bigger than b");
+// }
+// console.log("code is done running.")
+
+
+// print the absolute value of a
+let a = -87;
+let b = -1;
+if (a < 0) {
+    a = a*b;
+    // console.log (a*b);
+} 
+console.log (a);
+
+
+
+
+// print the max of a and b
+let a = 76;
+let b = 76;
+if (b > a) {
+ console.log(b);
 } else {
-  console.log("a is smaller than or equal to b");
+  console.log(a);
 }
-console.log("code is done running.")
 
 
-// // print whether a is positive or negative
-// let a = 5;
+// if (b > a) {
+//   console.log(b);
+// }
 
-
-// // print the absolute value of a
-// let a = -87;
-
-
-// // print the smaller of a and b
-// let a = 5;
-// let b = 3;
+// if (a >= b) {
+//   console.log(a);
+//  }
+ 
 
 
 /////////// boolean and ///////////
@@ -75,9 +184,6 @@ console.log("code is done running.")
 
 
 // do the previous code, but input the user and password
-// remember...
-// let a = input("what's your name?: ");
-// console.log("hello", a + "!");
 
 
 // do the previous code, input the user and password,
